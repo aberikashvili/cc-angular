3e363c6bf16583c2aa92adf32de75940af35f1c2
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-04-24 01:51:40 +0400

    [seciton-my-partner]
    Created HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
 
 
 @NgModule({
-  declarations: [AppComponent,],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
