8d3a939692de03e66d5b5746051751c37199c73d
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-04-24 17:02:20 +0400

    Button component
    add button component with enum technology.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AwardsDeedsSectionComponent } from './awards-deeds-section/awards-deeds-section.component';
+import { ButtonsComponent } from './buttons/buttons/buttons.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     AwardsDeedsSectionComponent
+    ButtonsComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/buttons/button.type.ts
@@ -0,0 +1,5 @@
+export enum buttonType 
+{
+    Primary = "primary",
+    Secondary = "secondary"
+}
