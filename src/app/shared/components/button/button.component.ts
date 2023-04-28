2af43ea58a7e28f7a7b6563457851f81edfb180d
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-04-28 11:59:13 +0400

    Updated and created new ngClass name for btn
    Updated button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { Component, ChangeDetectionStrategy, Input, Output,EventEmitter } from '@angular/core';
-export type ButtonType = 'primary' | 'secondary'
+export type ButtonType = 'primary' | 'secondary' | 'awardsbtn'
 
 @Component({
   selector: 'app-button',
@@ -22,6 +22,10 @@ export class ButtonsComponent  {
 
   onClick() {
     this.clicked.emit();
+  }
+
+  get awardsBtn():boolean {
+    return this.type==="awardsbtn";
   }
 
   
