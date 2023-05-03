9a04ee3da0dd14054fd8d7b2a1409511769bcb2d
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-03 17:56:19 +0400

    Working on creating form

--- src/app/modules/home/components/need-our-help-section/need-our-help-section.component.ts
+++ src/app/modules/home/components/need-our-help-section/need-our-help-section.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-need-our-help-section',
   templateUrl: './need-our-help-section.component.html',
   styleUrls: ['./need-our-help-section.component.scss']
 })
-export class NeedOurHelpSectionComponent {
-  formGroup: FormGroup;
+export class NeedOurHelpSectionComponent implements OnInit {
+  name!: string;
+  number!: number;
+  email!: string;
+  description!: string;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.formGroup = this.formBuilder.group({
-      name: ['', Validators.required],
-      number: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      description: ['', Validators.required]
-    });
+
+  constructor() {
+
+  }
+  // myForm!: FormGroup;
+
+  submitForm(form: NgForm) {
+    // console.log('Name:', this.name);
+    // console.log('Number:', this.number);
+    // console.log('Email:', this.email);
+    // console.log('Description:', this.description);
+    console.log(form.value);
+    
   }
 
-  onSubmit() {
-    console.log(this.formGroup.value);
+
+  ngOnInit(): void {
+    // this.myForm = this.FormBuilder.group({
+    //   name: ['', Validators.required],
+    //   number: ['', Validators.required],
+    //   email: ['', [Validators.required, Validators.email]],
+    //   description: ['', Validators.required]
+    // });
   }
+
+  // onSubmit() {
+  //   console.log(this.myForm.value);
+  // }
 }
+
+
