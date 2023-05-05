e4112e414cf257555ebe4a566e738b465900aeef
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-05 17:04:51 +0400

    UUpdated home component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AwardsDeedsSectionComponent } from './components/awards-deeds-section/awards-deeds-section.component';
 import { MyPartnersSectionComponent } from './components/my-partners-section/my-partners-section.component';
 import { NeedOurHelpSectionComponent } from './components/need-our-help-section/need-our-help-section.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AboutUsSectionComponent } from './components/about-us-section/about-us-section.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AwardsDeedsSectionComponent } from './components/awards-deeds-section/awards-deeds-section.component';
 
 const routes: Routes = [
   {
@@ -19,5 +19,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [HomeComponent, AwardsDeedsSectionComponent, MyPartnersSectionComponent, AboutUsSectionComponent,  NeedOurHelpSectionComponent, FooterComponent],
   imports: [RouterModule.forChild(routes), SharedModule, ReactiveFormsModule],
+
 })
 export class HomeModule {}
