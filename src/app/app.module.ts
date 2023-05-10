9c1a00f0bfee560da669d29c59f90ff43bb73c94
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-10 15:04:31 +0400

    Created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OurTeamSectionComponent } from './modules/our-team-section/our-team-section.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, OurTeamSectionComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/modules/our-team-section/our-team-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-team-section',
+  templateUrl: './our-team-section.component.html',
+  styleUrls: ['./our-team-section.component.scss']
+})
+export class OurTeamSectionComponent {
+
+}
