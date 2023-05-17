6f2c6be5d38dc084a3441a3f25fd7c711ba74c61
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-17 15:13:01 +0400

    Created partners component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YourReliablePartnersSectionComponent } from './modules/your-reliable-partners-section/your-reliable-partners-section.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, YourReliablePartnersSectionComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/modules/your-reliable-partners-section/your-reliable-partners-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-reliable-partners-section',
+  templateUrl: './your-reliable-partners-section.component.html',
+  styleUrls: ['./your-reliable-partners-section.component.scss']
+})
+export class YourReliablePartnersSectionComponent {
+
+}
