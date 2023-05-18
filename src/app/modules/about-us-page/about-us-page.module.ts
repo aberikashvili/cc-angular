e5f1e1a029fc6eb9643f516aa91616a0fc28a286
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-18 13:58:30 +0400

    Updated imports in new folder

--- src/app/modules/about-us-page/about-us-page.module.ts
+++ src/app/modules/about-us-page/about-us-page.module.ts
@@ -3,6 +3,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { AboutUsPageComponent } from './about-us-page.component';
 import { OurTeamSectionComponent } from './components/our-team-section/our-team-section.component';
+import { YourReliablePartnersSectionComponent } from './components/your-reliable-partners-section/your-reliable-partners-section.component';
+import { AwardsDeedsSectionSecondComponent } from './components/awards-deeds-section-second/awards-deeds-section-second.component';
 
 const routes: Routes = [
   {
@@ -12,7 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AboutUsPageComponent, OurTeamSectionComponent],
+  declarations: [
+    AboutUsPageComponent,
+    OurTeamSectionComponent,
+    YourReliablePartnersSectionComponent,
+    AwardsDeedsSectionSecondComponent,
+  ],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class AboutUsPageModule {}
