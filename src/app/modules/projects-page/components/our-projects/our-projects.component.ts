394e1b28da3d1eb3405a28cdf06d54213eebcda1
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-24 03:17:31 +0400

    Created list where i put all this information and generated with ngFor

--- src/app/modules/projects-page/components/our-projects/our-projects.component.ts
+++ src/app/modules/projects-page/components/our-projects/our-projects.component.ts
@@ -3,8 +3,61 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-our-projects',
   templateUrl: './our-projects.component.html',
-  styleUrls: ['./our-projects.component.scss']
+  styleUrls: ['./our-projects.component.scss'],
 })
 export class OurProjectsComponent {
+  List: any[] = [];
 
+  ngOnInit() {
+    this.getList();
+  }
+
+  getList() {
+    this.List = [
+      {
+        first: {
+          img: 'assets/img/parlament.jpg',
+          stage: 'დასრულებული',
+          name: 'ქუთაისის პარლამენტი',
+        },
+        second: {
+          img: 'assets/img/parlament.jpg',
+          stage: 'დასრულებული',
+          name: 'ქუთაისის პარლამენტი',
+        },
+        third: {
+          img: 'assets/img/parlament.jpg',
+          stage: 'დასრულებული',
+          name: 'ქუთაისის პარლამენტი',
+        },
+        fourth: {
+          img: 'assets/img/parlament.jpg',
+          stage: 'დასრულებული',
+          name: 'ქუთაისის პარლამენტი',
+        },
+      },
+      // {
+      //   first: {
+      //     img: 'assets/img/parlament.jpg',
+      //     stage: 'დასრულებული',
+      //     name: 'ქუთაისის პარლამენტი',
+      //   },
+      //   second: {
+      //     img: 'assets/img/parlament.jpg',
+      //     stage: 'დასრულებული',
+      //     name: 'ქუთაისის პარლამენტი',
+      //   },
+      //   third: {
+      //     img: 'assets/img/parlament.jpg',
+      //     stage: 'დასრულებული',
+      //     name: 'ქუთაისის პარლამენტი',
+      //   },
+      //   fourth: {
+      //     img: 'assets/img/parlament.jpg',
+      //     stage: 'დასრულებული',
+      //     name: 'ქუთაისის პარლამენტი',
+      //   },
+      // },
+    ];
+  }
 }
