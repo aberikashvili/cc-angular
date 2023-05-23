f8d8c3fced7abdc36ab3fa4ee2c7ec4f2084966a
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-05-23 16:53:57 +0400

    added soc-media icons

--- src/app/modules/contact-us-page/contact-us-page-module.ts
+++ src/app/modules/contact-us-page/contact-us-page-module.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { NgModule } from '@angular/core';
 import { ContactUsPageComponent } from './contact-us-page.component';
-
+import { ContactUsSectionComponent } from './components/contact-us-section/contact-us-section.component';
 const routes: Routes = [
   {
     path: '',
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [ContactUsPageComponent,],
+  declarations: [ContactUsPageComponent,ContactUsSectionComponent],
   imports: [RouterModule.forChild(routes), SharedModule],
 })
 export class ContactUsPageModule {}
