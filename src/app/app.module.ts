3600bc4a8def640086078d953a62165ff61d9e90
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-05-23 16:45:50 +0400

    Created  Components where i should work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsPageComponent } from './modules/projects-page/projects-page.component';
+import { OurProjectsComponent } from './modules/projects-page/components/our-projects/our-projects.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProjectsPageComponent, OurProjectsComponent],
   imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
