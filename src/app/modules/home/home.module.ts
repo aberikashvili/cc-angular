3bbbac6f38b2ac38798bb5be3dff545880662826
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-05-24 13:45:13 +0400

    .

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -7,7 +7,6 @@ import { NeedOurHelpSectionComponent } from './components/need-our-help-section/
 import { ReactiveFormsModule } from '@angular/forms';
 import { AboutUsSectionComponent } from './components/about-us-section/about-us-section.component';
 import { AwardsDeedsSectionComponent } from './components/awards-deeds-section/awards-deeds-section.component';
-import { AwardsDeedsSectionSecondComponent } from './components/awards-deeds-section-second/awards-deeds-section-second.component';
 import { ContactUsSectionComponent } from '../contact-us-page/components/contact-us-section/contact-us-section.component';
 const routes: Routes = [
   {
@@ -24,8 +23,7 @@ const routes: Routes = [
     AboutUsSectionComponent,
     NeedOurHelpSectionComponent,
     ContactUsSectionComponent,
-    AwardsDeedsSectionSecondComponent,
-    ContactUsSectionComponent
+    
   ],
   imports: [RouterModule.forChild(routes), SharedModule, ReactiveFormsModule],
 })
