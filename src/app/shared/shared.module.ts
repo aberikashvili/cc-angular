5de5c651b2a97af18c183b372aa86690de8586e7
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-05-25 16:08:10 +0400

    edited

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,12 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
 import { ButtonsComponent } from './components/button/button.component';
 import { HeaderComponent } from './components/header/header.component';
 import { WidgetComponent } from './components/widget/widget.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [ButtonsComponent, HeaderComponent, WidgetComponent],
+  declarations: [ButtonsComponent, HeaderComponent, WidgetComponent,FooterComponent],
   imports: [CommonModule, CarouselModule],
-  exports: [CommonModule, ButtonsComponent, HeaderComponent, WidgetComponent,CarouselModule],
+  exports: [CommonModule, ButtonsComponent, HeaderComponent, WidgetComponent,CarouselModule,FooterComponent],
 })
 export class SharedModule {
 }
