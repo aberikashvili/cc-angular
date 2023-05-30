ebe0c14689b39eefbaa9595414255b6c378f8bff
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-05-30 15:11:29 +0400

    make owl carousel

--- src/app/modules/projects-page/components/photo-gallery-section/photo-gallery-section.component.ts
+++ src/app/modules/projects-page/components/photo-gallery-section/photo-gallery-section.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+  import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-photo-gallery-section',
   templateUrl: './photo-gallery-section.component.html',
-  styleUrls: ['./photo-gallery-section.component.scss']
+  styleUrls: ['./photo-gallery-section.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PhotoGallerySectionComponent {
+export class PhotoGallerySectionComponent implements AfterViewInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['prev', 'next'],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 2,
+      },
+      740: {
+        items: 3,
+      },
+      940: {
+        items: 4,
+      },
+    },
+    nav: true,
+  };
 
+  card = [
+    {
+      image:'assets/images/header-logo.png',
+    }
+    
+  ]
+
+  ngAfterViewInit(): void {}
 }
