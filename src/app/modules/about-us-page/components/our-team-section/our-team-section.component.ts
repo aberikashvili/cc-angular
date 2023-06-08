1347386b1add3ed73f58e8f9764197bbd374df8b
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-06-08 16:48:33 +0400

    Finished about-us our team

--- src/app/modules/about-us-page/components/our-team-section/our-team-section.component.ts
+++ src/app/modules/about-us-page/components/our-team-section/our-team-section.component.ts
@@ -5,4 +5,111 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   templateUrl: './our-team-section.component.html',
   styleUrls: ['./our-team-section.component.scss'],
 })
-export class OurTeamSectionComponent {}
+export class OurTeamSectionComponent {
+  sliderData = [
+    {
+      left: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      middle: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      right: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      last: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+    },
+    {
+      left: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      middle: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      right: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      last: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+    },
+    {
+      left: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      middle: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      right: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      last: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+    },
+    {
+      left: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      middle: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      right: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+      last: {
+        img: 'assets/img/box.jpg',
+        NameSurname: 'გოჩა ვაშაკიძე',
+        title: 'საწყობის უფროსი',
+        height: '385px',
+      },
+    },
+  ];
+}
