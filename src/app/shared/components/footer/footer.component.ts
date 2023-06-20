202ce98619b72fcab76a596a5a74d989ba097068
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-06-20 15:24:23 +0400

    instaled angular material and accordion

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  items = ['თბილი სახლი', 'სერვისები', 'სწრაფი ლინკები'];
+  expandedIndex = 0;
 }
