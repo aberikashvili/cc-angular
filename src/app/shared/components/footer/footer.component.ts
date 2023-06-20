a375b33ee9bda95cd00c3e80a463aacd976b2f5b
NikaBeruashvilii <nika.beruashvili.1@btu.edu.ge>
2023-06-20 15:53:46 +0400

    Updated accordion

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 export class FooterComponent {
   items = ['თბილი სახლი', 'სერვისები', 'სწრაფი ლინკები'];
   expandedIndex = 0;
+
+  menu = [
+    {
+      menuItem1: 'ჩვენ შესახებ',
+      menuItem2: 'თანამშრომლები',
+      menuItem3: 'კონტაქტი',
+    },
+    {
+      menuItem1: 'სუსტი დენები',
+      menuItem2: 'მექანიკური სისტემები',
+      menuItem3: 'წყალგაყვანილობის სისტემები',
+      menuItem4: 'ამწე-სატრანსპორტო სისტემები',
+      menuItem5: 'ელექტროობა',
+    },
+    {
+      menuItem1: 'ჯილდოები',
+      menuItem2: 'პარტნიორები',
+      menuItem3: 'პროექტები',
+    },
+  ];
 }
