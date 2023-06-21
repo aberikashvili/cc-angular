3e6e27eedc9a068e495e06d7fd4b9765f1410f19
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-06-21 13:01:23 +0400

    created form

--- src/app/modules/home/components/test-section/test-section.component.ts
+++ src/app/modules/home/components/test-section/test-section.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-test-section',
   templateUrl: './test-section.component.html',
-  styleUrls: ['./test-section.component.scss']
+  styleUrls: ['./test-section.component.scss'],
 })
-export class TestSectionComponent {
+export class TestSectionComponent implements OnInit {
+  myForm!: FormGroup;
 
+  constructor(private _formBuilder: FormBuilder) {}
+  
+  ngOnInit() {
+    this.myForm = this._formBuilder.group({
+      nameSurname: ['', Validators.required],
+      number: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+
+  onClick() {
+    console.log(this.myForm.value);
+  }
 }
