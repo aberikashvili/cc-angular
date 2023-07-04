38a5716c5da3ddb04b0236f4480879ac502bbe70
Gega-Rostomashvili <99957721+Gega-Rostomashvili@users.noreply.github.com>
2023-07-04 16:02:47 +0400

    header mobile cerated

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isShow: Boolean = false;
+  onBurgerShow() {
+    this.isShow = !this.isShow;
+    console.log(this.isShow);
+  }
+}
